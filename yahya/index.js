09b491477cbf912e60d531cc57598beb4924ce8a
yahya5544266 <118754624+yahya5544266@users.noreply.github.com>
2022-11-21 20:58:28 +0500

    Add files via upload

---
+++ yahya/index.js
@@ -0,0 +1,32 @@
+var valu_1 = 3;
+var valu_2 = 2;
+var value_3 = "*";
+
+
+if ( value_3=="*"){
+console.log(valu_1+valu_2);
+}
+else if( value_3=="*"){
+console.log(valu_1-valu_2);
+}
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
